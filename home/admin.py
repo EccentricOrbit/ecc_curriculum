45b513ce5be6bc201c34aaf857205e31ecd59b0f
Michael Horn <michael-horn@northwestern.edu>
2022-12-18 22:22:48 -0600

    added admin view for PlaylistNodes

--- home/admin.py
+++ home/admin.py
@@ -4,14 +4,21 @@ from django.contrib import admin
 from martor.widgets import AdminMartorWidget
 from martor.models import MartorField
 
-from home.models import PostMeta, Post
+from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
+
+from home.models import PostMeta, Post, PlaylistNode
 
 
 class PostMetaAdminInline(admin.TabularInline):
     model = PostMeta
 
-
 class PostAdmin(admin.ModelAdmin):
     list_display = ["title", "id"]
 
+class PlaylistNodeAdmin(TreeAdmin):
+    #formfield_overrides = { models.TextField: {'widget': AdminMartorWidget} }
+    form = movenodeform_factory(PlaylistNode)
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(PlaylistNode, PlaylistNodeAdmin)
