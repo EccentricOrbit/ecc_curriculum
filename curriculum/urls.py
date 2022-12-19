a402b4244c47e24ab974602b31b649144dc93df1
Michael Horn <michael-horn@northwestern.edu>
2022-12-19 10:05:34 -0600

    serve home app from root

--- curriculum/urls.py
+++ curriculum/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
-    path('home/', include('home.urls')),
+    path('', include('home.urls')),
     path('admin/', admin.site.urls),
     path('martor/', include('martor.urls')),
 ]
