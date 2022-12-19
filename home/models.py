1ac34ce3d89a76c59a5c1af5a401ea67755caf1d
Michael Horn <michael-horn@northwestern.edu>
2022-12-18 22:24:04 -0600

    added PlaylistNode to models

--- home/models.py
+++ home/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from martor.models import MartorField
+from treebeard.al_tree import AL_Node
 
 User = get_user_model()
 
@@ -33,3 +34,34 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
+
+
+# This is the treebeard hierarchy. Uses adjacency list algorithm.
+class PlaylistNode(AL_Node):
+    parent = models.ForeignKey('self',
+                               related_name='children_set',
+                               on_delete=models.CASCADE,
+                               default=None,
+                               blank=True,
+                               null=True,
+                               db_index=True)
+    node_order_by = ['ordering', 'title']
+    ordering = models.IntegerField()
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(null=False, unique=True)
+
+    # tunepad project_id to display an embedded project
+    project_id = models.IntegerField(null=True, blank=True, default=None, verbose_name = 'TunePad Project ID')
+
+    # URL to display external content (e.g. Google doc)
+    url = models.URLField(max_length=512, default=None, blank=True, null=True, verbose_name = 'External URL')
+
+    # markdown text to display markdown
+    text = models.TextField(verbose_name = 'Markdown Text', default=None, blank=True, null=True)
+    # MartorField?
+
+    def __str__(self):
+        return "{} ({})".format(self.title, self.id)
+
+    def get_absolute_url(self):
+        return reverse("content", kwargs={"slug": self.slug})
