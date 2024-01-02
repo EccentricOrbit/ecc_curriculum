b12689d342dbf575bb9e6807345b7aae23786864
Michael Horn <michael-horn@northwestern.edu>
2024-01-02 13:05:41 -0600

    11ty

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const { execSync } = require('child_process')
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(syntaxHighlight, {
+        alwaysWrapLineHighlights: true,
+    });
+    eleventyConfig.addPassthroughCopy({ "assets/js" : "js" });
+    eleventyConfig.addPassthroughCopy({ "assets/css" : "css" });
+    eleventyConfig.addPassthroughCopy({ "assets/sounds" : "sounds" });
+    eleventyConfig.addPassthroughCopy({ "assets/images" : "images" });
+    eleventyConfig.addPassthroughCopy({ "assets/webfonts" : "webfonts" });
+
+    eleventyConfig.on('eleventy.after', () => {
+        execSync(`npx pagefind --site _site --glob \"**/*.html\"`, { encoding: 'utf-8' })
+    });
+};
+
+async function wait(delay = 2000) {
+    return new Promise((resolve) => setTimeout(resolve, delay));
+}
