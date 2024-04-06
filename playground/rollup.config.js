702c7d7b81da94dba1762989b3ab6bf53d4e24ee
Michael Horn <michael-horn@northwestern.edu>
2024-04-06 12:12:44 -0500

    added terser, only copy files once

--- playground/rollup.config.js
+++ playground/rollup.config.js
@@ -5,23 +5,33 @@ import serve from 'rollup-plugin-serve'
 import livereload from 'rollup-plugin-livereload'
 import {cssModules} from 'rollup-plugin-css-modules'
 import html from 'rollup-plugin-html';
+import terser from '@rollup/plugin-terser';
 
 export default {
-    input: 'src/main.ts',
-    output: {
+    input: 'src/tunepad.ts',
+    output: [{
         dir: 'build/js',
-        format: 'cjs'
+        format: 'cjs',
+        sourcemap: true
     },
+    {
+        file : 'build/js/tunepad.min.js',
+        format : 'cjs',
+        name : 'version',
+        plugins : [terser ()]
+    }],
     plugins: [
         html({include: '**/*.module.html'}),
         cssModules(),
-        typescript(),
+        typescript({ sourceMap: true }),
         copy({
             watch: [ 'src/index.html', 'src/assets' ],
             targets: [
                 { src: 'src/index.html', dest: 'build' },
-                { src: 'src/assets/css/**/*', dest: 'build/css' }
+                { src: 'src/assets', dest: 'build' }
             ],
+            flatten : false,
+            copyOnce : true,
             outputFolder: 'build'
         }),
         serve('build'),
