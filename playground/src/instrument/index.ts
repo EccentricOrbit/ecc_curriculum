461eef706ba9494a7c126135557c1c4d1cd6a9b4
Michael Horn <michael-horn@northwestern.edu>
2024-04-08 12:34:32 -0500

    restoring

--- playground/src/instrument/index.ts
+++ playground/src/instrument/index.ts
@@ -11,44 +11,4 @@
  * of the National Science Foundation (NSF).
  */
 
-/**
- * An instrument is a virtual musical instrument that can be played with keyboard, mouse,
- * touchscreen, or attached midi device. Instruments use shadow dom to encapsulate
- * stylesheets and element names. Interaction with an instrument generates custom events
- * that can be subscribed to.
- */
-export abstract class Instrument {
-
-}
-
-export type NoteSource = "pointer" | "keyboard" | "midi" | "system";
-
-export interface NoteProperties {
-    note? : number,         // midi note value (0 - 127)
-    velocity? : number,     // midi note velocity (0 - 127)
-    value? : number,        // value of pitch bend or control
-    time? : number,
-    message : "note-on" | "note-off" | "pitch-bend",
-    source : NoteSource
-}
-
-const NoteDefaults : NoteProperties = {
-    note : 0,
-    velocity : 90,
-    message : "note-on",
-    source : "pointer"
-}
-
-/**
- * Note event class
- */
-export class NoteEvent {
-
-    readonly props : NoteProperties;
-
-    get customEvent() : CustomEvent { return new CustomEvent(this.props.message, { detail: this.props }); }
-    
-    constructor(props : NoteProperties) {
-        this.props = { ...NoteDefaults, ...props, ...{ time: Date.now() } };
-    }
-}
+export * from './piano';
