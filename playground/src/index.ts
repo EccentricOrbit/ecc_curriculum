88cd882bb7fbb1ee33225ca0cacbd1403fabd915
Michael Horn <michael-horn@northwestern.edu>
2024-04-09 18:03:29 -0500

    created new custom element

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,7 +1,8 @@
 import { PianoKeyboard } from "./instrument";
+import { NoteExplorer } from "./interactives/note-explorer";
+
+customElements.define(PianoKeyboard.ELEMENT, PianoKeyboard);
+customElements.define(NoteExplorer.ELEMENT, NoteExplorer);
 
 export * from './synthesizer';
 export * from './core/';
-
-customElements.define(PianoKeyboard.ELEMENT, PianoKeyboard);
-
