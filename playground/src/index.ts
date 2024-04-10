7a2d6d89f5dd36a3d9a6c923d16826f42a0314c0
Michael Horn <michael-horn@northwestern.edu>
2024-04-09 23:23:29 -0500

    added playable piano instrument

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,8 +1,9 @@
-import { PianoKeyboard } from "./instrument";
+import { Piano, PianoKeyboard } from "./instrument";
 import { NoteExplorer } from "./interactives/note-explorer";
 
 customElements.define(PianoKeyboard.ELEMENT, PianoKeyboard);
 customElements.define(NoteExplorer.ELEMENT, NoteExplorer);
+customElements.define(Piano.ELEMENT, Piano);
 
 export * from './synthesizer';
 export * from './core/';
