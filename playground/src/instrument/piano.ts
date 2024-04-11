3877d0446dea5be032a3ce96387516c2ce661909
Michael Horn <michael-horn@northwestern.edu>
2024-04-11 17:33:05 -0500

    passthrough keyboard attributes

--- playground/src/instrument/piano.ts
+++ playground/src/instrument/piano.ts
@@ -14,18 +14,29 @@ import styles from './piano.module.css' with {type: 'css'};
 import html from './piano.module.html';
 import { Note, TunePadAudio } from '../core';
 import { Synthesizer } from '../synthesizer';
+import { PianoKeyboard } from './keyboard';
 
 /**
  * Playable piano keyboard with note names and numbers.
  * Octave buttons.
  * 
- * <piano-instrument patch="grand-piano/patch.json"></piano-instrument>
+ * <piano-instrument
+ *      patch="grand-piano/patch.json">
+ *      note-hints = "true"   // show note names on keys
+ *      midi-hints = "true"   // show midi note numbers on keys
+ *      armed = "false"       // listen for keyboard events
+ *      min-octave = "0"      // lowest octave available
+ *      max-octave = "7"      // highest octave available
+ *      key-range = "28"      // how many keys to show at one time
+ *      focus-octave = "3">   // focus (left-most) octave showing
+ * </piano-instrument>
  */
 export class Piano extends HTMLElement {
 
     static readonly ELEMENT = "piano-instrument";
 
     static observedAttributes = [ 
+        ... PianoKeyboard.observedAttributes,
         "patch",  // URL for patch JSON file
     ];
 
@@ -35,47 +46,54 @@ export class Piano extends HTMLElement {
     // TunePad audio core
     private audio : TunePadAudio;
 
+    // <piano-keyboard> element
+    private keyboard : HTMLElement;
+
     // create a synthesizer and load a grand piano
     private synth = new Synthesizer();
 
     private octave = 3;
+    private minOctave = 0;
+    private maxOctave = 7;
 
     constructor() {
         super();
         this.root = this.attachShadow({ mode: 'open' });
         this.root.adoptedStyleSheets.push(styles);
-        this.audio = TunePadAudio.init();
-    }
-
-
-    connectedCallback() {
         const template = document.createElement('template');
         template.innerHTML = html;
 
         // Create a shadow root
         this.root.appendChild(template.content.cloneNode(true));
 
+        this.keyboard = this.root.querySelector('piano-keyboard') as HTMLElement;
+
+        this.audio = TunePadAudio.init();
+    }
+
+
+    connectedCallback() {
         // create the audio context
         this.audio = TunePadAudio.init();
 
         // catch piano note events and play note + display note info
-        this.root.querySelector('piano-keyboard')?.addEventListener('note-on', (e) => {
+        this.keyboard.addEventListener('note-on', (e) => {
             const note = new Note((e as CustomEvent).detail.note);
             this.synth.playNote(note, this.audio.context.destination);
         });
 
-        this.root.querySelector('piano-keyboard')?.addEventListener('note-off', (e) => {
+        this.keyboard.addEventListener('note-off', (e) => {
             const note = new Note((e as CustomEvent).detail.note);
             this.synth.releaseNote(note);
         });
 
         this.root.querySelector('#down-octave')?.addEventListener('click', (e) => {
-            this.octave = Math.max(1, this.octave - 1);
+            this.octave = Math.max(this.minOctave, this.octave - 1);
             this.root.querySelector('piano-keyboard')?.setAttribute('focus-octave', `${this.octave}`);
         });
 
         this.root.querySelector('#up-octave')?.addEventListener('click', (e) => {
-            this.octave = Math.min(8, this.octave + 1);
+            this.octave = Math.min(this.maxOctave, this.octave + 1);
             this.root.querySelector('piano-keyboard')?.setAttribute('focus-octave', `${this.octave}`);
         });
     }
@@ -89,5 +107,18 @@ export class Piano extends HTMLElement {
         if (name === 'patch' && newValue !== oldValue) {
             this.synth.loadPatch(newValue, this.audio.context);
         }
+        else if (newValue !== oldValue) {
+            this.keyboard.setAttribute(name, newValue);
+        }
+        if (name === 'focus-octave') {
+            this.octave = parseInt(newValue);
+        } else if (name === 'min-octave') {
+            this.minOctave = parseInt(newValue);
+        } else if (name === 'max-octave') {
+            this.maxOctave = parseInt(newValue);
+        }
+        this.octave = isNaN(this.octave) ? 3 : this.octave;
+        this.minOctave = isNaN(this.minOctave) ? 0 : this.minOctave;
+        this.maxOctave = isNaN(this.maxOctave) ? 7 : this.maxOctave;
     }
 }
